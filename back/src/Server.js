49662dafb4c660740943988c2611e5347a540b89
thiago <thiago41962@fiec.edu.br>
2026-06-11 15:07:43 -0300

    server.js melhorado, port vindo como undefined

--- back/src/Server.js
+++ back/src/Server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const cors = require("cors");
 require('dotenv').config();
 
+const PORT = process.env.PORT
+console.log(process.env.PORT)
+
+const livroRoute = require('./Routes/livroRoute');
+
 const app = express();
 app.use(cors())
-app.use('/livros', livroRoute);
-app.use(express.json());
 
-app.listen(process.env.PORT, () => console.log("Servidor rodando na porta 3000"))
+app.use(express.json());
+app.use('/livros', livroRoute);
+
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
