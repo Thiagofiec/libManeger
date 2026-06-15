7bdb697bd3170adc8b5c701d215885aeb2e457ee
thiago <thiago41962@fiec.edu.br>
2026-06-15 17:35:42 -0300

    modal de usuarios

--- back/src/Modals/UsuarioModel.js
+++ back/src/Modals/UsuarioModel.js
@@ -2,128 +2,57 @@ const BD = require('../config/BD')
 
 // pegar todos usuarios
 
-exports.getTodosUsuarios = async (req,res) => {
-    try {
+exports.getTodos = async () => {
         const [rows] = await  BD.query(
             'SELECT * FROM Usuario'
         )
 
-        res.json(rows)
-    } catch (error) {
-        console.error('Database query error:', error);
-
-        res.status(500).json({
-            erro: 'Erro interno do servidor'
-        })
-    }
+        return rows
 }
+
 // pegar usuario por id
 
-exports.getUuarioId = async (req,res) => {
-    try {
-        const { id } = req.params 
+exports.getId = async (id) => {
         const [rows] = await BD.query(
-            'SELECT * from livro where id_usuario = ?',
+            'SELECT * from Usuario where id_usuario = ?',
             [id]
         )
-        if (rows.length === 0) {
-            return res.status(404).json({
-                erro: 'Usuario não encontrado'
-            });
-}
-        res.json(rows[0])
-    } catch (error) {
-        console.error('Database query error:', error);
+        return rows 
+    } 
 
-        res.status(500).json({
-            erro: 'Erro interno do servidor'
-        })
-    }
-}
 
 // cadastrar usuario
 
-exports.cadastrarUsuario = async (req,res) => {
-    try {
-        const {nome, email, telefone, cpf, senha, tipo} = req.body
+exports.cadastrar = async (nome,email,telefone,cpf,senha,tipo) => {
+    const [resultado] = await BD.query(
+        'insert into Usuario(nome,email,telefone,cpf,senha_hash,tipo,ativo) values(?,?,?,?,?,?,?,1)',
+        [nome,email,telefone,cpf,senha,tipo]
+    )
 
-        resultado = await BD.query(
-            'insert into Usuario(nome,email,telefone,cpf,senha_hash,tipo,ativo) values(?,?,?,?,?,?,1)',
-            [nome,email,telefone,cpf,senha,tipo]
-        )
-
-         if (resultado.affectedRows === 0) {
-            return res.status(404).json({
-                erro: 'Falha a cadastrar usuario'
-            });
-        }
-        
-        res.json({
-            mensagem: 'Livro cadastrado com sucesso'
-        });
-    } catch (error) {
-        console.error('Database query error:', error);
-
-        res.status(500).json({
-            erro: 'Erro interno do servidor'
-        })
-    }
+    return resultado      
 }
 
 //desativar usuario
 
-exports.destivarUsuario = async (req,res) => {
-    try {
-        const { id } = req.params 
+exports.desativar = async (id) => {
 
-        const [resultado] = await BD.query(
-            'update Usuario set ativo = 0 where id_usuario = ?',
-            [id]
-        )
+    const [resultado] = await BD.query(
+        'update Usuario set ativo = 0 where id_usuario = ?',
+        [id]
+    )
 
-        if (resultado.affectedRows === 0) {
-            return res.status(404).json({
-                erro: 'Usuario não encontrado'
-            });
-        }
-
-        res.json({
-            mensagem: 'Usuario desativado com sucesso'
-        });
-    } catch (error) {
-        console.error('Database query error:', error);
-
-        res.status(500).json({
-            erro: 'Erro interno do servidor'
-        })
-    }
+    return resultado
 }
 
-// reativar usuario 
+//reativar usuario
 
-exports.reativarLivro = async (req,res) => {
-    try {
-        const { id } = req.params 
+exports.reativar = async (id) => {
 
-        const [resultado] = await BD.query(
-            'update Usuario set ativo = 1 where id_usuario = ?',
-            [id]
-        )
+    const [resultado] = await BD.query(
+        'update Usuario set ativo = 1 where id_usuario = ?',
+        [id]
+    )
 
-        if (resultado.affectedRows === 0) {
-            return res.status(404).json({
-                erro: 'Usuario não encontrado'
-            });
-        }
-
-        res.json({
-            mensagem: 'Usuario reativado com sucesso'
-        });
-    } catch (error) {
-        console.error('Database query error:', error);
-
-        res.status(500).json({
-            erro: 'Erro interno do servidor'
-        })
-    }
+    return resultado
 }
+      
