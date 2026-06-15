1cca4eb6858e6d89e62137328388ed47f2ab9066
thiago <thiago41962@fiec.edu.br>
2026-06-15 15:12:48 -0300

    mudando o routes do livro para ser um router(era isso oque me ferrou na prova )

--- back/src/Routes/LivroRoute.js
+++ back/src/Routes/LivroRoute.js
@@ -1,11 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
 const LivroCon = require("../Controllers/LivroController")
 
-module.exports = app => {
-    app.get('/',LivroCon.getTodosLivros)
-    app.get('/:id', LivroCon.getLivroId)
-    app.post('/cadastrar', LivroCon.cadastrarLivro)
-    app.patch('/:id/desativar', LivroCon.desativarLivro)
-    app.patch('/:id/reativar', LivroCon.reativarLivro)
-    app.patch('/:id/indisponibilizar', LivroCon.indisponibilizarLivro)
-    app.patch('/:id/disponibilizar', LivroCon.disponibilizarLivro)
-}
+router.get('/',LivroCon.getTodosLivros)
+router.get('/:id', LivroCon.getLivroId)
+router.post('/cadastrar', LivroCon.cadastrarLivro)
+router.patch('/:id/desativar', LivroCon.desativarLivro)
+router.patch('/:id/reativar', LivroCon.reativarLivro)
+router.patch('/:id/indisponibilizar', LivroCon.indisponibilizarLivro)
+router.patch('/:id/disponibilizar', LivroCon.disponibilizarLivro)
+
+module.exports = router;
