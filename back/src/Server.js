0298f7813e505be1aa1bfbf0158b336fed394de5
thiago <thiago41962@fiec.edu.br>
2026-06-16 16:38:17 -0300

    adicionando as rotas do emprestimo no server

--- back/src/Server.js
+++ back/src/Server.js
@@ -7,13 +7,15 @@ console.log(process.env.PORT)
 
 const livroRoute = require('./Routes/livroRoute');
 const usuarioRoute = require('./Routes/UsuarioRoute')
+const emprestimoRoute = require('./Routes/EmprestimoRoute')
 
 const app = express();
 app.use(cors())
 
 app.use(express.json());
 app.use('/livros', livroRoute);
-app.use('/usuarios', usuarioRoute)
+app.use('/usuarios', usuarioRoute);
+app.use('/emprestimo', emprestimoRoute)
 
 
 app.listen(porta, () => console.log(`Servidor rodando na porta ${porta}`))
