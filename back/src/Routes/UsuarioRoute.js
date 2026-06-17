f724e81888009276bd314a631d28f80dabf629d8
thiago <thiago41962@fiec.edu.br>
2026-06-17 17:59:23 -0300

    rota de login no usuario route

--- back/src/Routes/UsuarioRoute.js
+++ back/src/Routes/UsuarioRoute.js
@@ -8,5 +8,6 @@ router.get('/:id', UsuarioCon.getUsuarioId)
 router.post('/cadastrar', UsuarioCon.cadastrarUsuario)
 router.patch('/:id/desativar', UsuarioCon.desativarUsuario)
 router.patch('/:id/reativar', UsuarioCon.reativarUsuario)
+router.post('/login', UsuarioCon.entrarUsuario)
 
 module.exports = router;
